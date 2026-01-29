f09d568f007461358d074bcdd1bfc81c18a163ab
vivi2705-ui <jeenoria691@gmail.com>
2026-01-29 23:41:36 +0000

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+import React from 'react'
+import { useState } from "react";
+import Fiche from "../component/Fiche";
+import './globals.css'; 
+
+
+const page = () => {
+  const [mail, setmail] = useState("");
+
+  const handleClick = () => {
+    alert(mail);
+  }
+  return (
+    
+    
+    <div className="container">
+      <img src={"/OIP.webp"} className="img"/>
+
+      <section>
+        <Fiche
+        nom = {"SAWADOGO"}
+        prenom={"Jerielle"}
+        genre={"Female"}
+        competence={["React","Bootstrap","Java","Next"]}
+        />
+
+      </section>
+
+      <div className="form-section">
+        <span>Adresse mail</span>
+        <input 
+        type="text"
+        placeholder="Email"
+        value={mail}
+        onChange={(e) => setmail(e.target.value)} 
+        />
+        <button onClick={handleClick}>Click</button>
+       </div>
+
+
+      </div>
+
+  )
+}
+export default page;
